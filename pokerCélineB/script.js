5b5639e14cbcaeb42a188333d6dcdfd5da025b80
CelineBarbe <laloutrecosmique@hotmail.fr>
2021-10-15 11:49:37 +0200

    trop moche mais ça marche : afficher la main d'un joueur avec le tapis

--- pokerCélineB/script.js
+++ pokerCélineB/script.js
@@ -38,7 +38,7 @@ function deal (numberOfCards) {
         take = cards.pop();
         hand.push(take);
     }
-    return hand
+    return hand;
 }
 
 const playerOne = deal(2);
@@ -48,5 +48,47 @@ console.log("Il reste les cartes suivantes dans la pioche : " + cards);
 console.log("Le joueur un a en main les cartes suivantes : " + playerOne);
 console.log("Le joueur deux a en main les cartes suivantes : " + playerTwo);
 
+// le flop : tirer 5 cartes sur le modèle : Premier tour = 1 carte brulés, 3 cartes sorties; Deuxième tour = 1 carte brulés, 1 carte sortie; Troisième tour = 1 carte brulés, 1 carte sortie.
+//Utiliser la fonction deal?
+
+function flop(cards) {
+  console.log("Premier tour")
+  var burned = [];
+  var table = [];
+  firstCard = cards.pop();
+  burned.push(firstCard);
+  secondCard = cards.pop();
+  thirdCard = cards.pop();
+  fourthCard = cards.pop();
+  table.push(secondCard, thirdCard, fourthCard);
+  console.log("Sur la table : " + table);
+  console.log("Deuxième tour");
+  fifthCard = cards.pop();
+  burned.push(fifthCard);
+  sixthCard = cards.pop();
+  table.push(sixthCard);
+  console.log("Sur la table : " + table);
+  console.log("Troisième tour");
+  seventhCard = cards.pop();
+  burned.push(seventhCard);
+  eighthCard = cards.pop();
+  table.push(eighthCard);
+  console.log("Sur la table : " + table);
+
+  return table;
+}
+
+var tapis = flop(cards);
+//niveau 4 on crée des mains des joueurs qui comprennent leur tirage et le flop.
+
+function showdown(tapis, player) {
+  var newHand = tapis + player;
+  console.log("Main du joueur avec le tapis " + newHand);
+
+  return newHand;
+};
+
+showdown(tapis, playerOne);
+showdown(tapis, playerTwo);
 
 // faire des cartes sous forme d'objets avec valeur, type, représentation pour faciliter la vérification.
