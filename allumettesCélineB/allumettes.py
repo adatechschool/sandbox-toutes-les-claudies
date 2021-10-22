a09e3660860fd86717605c4c17c75268df60da5c
CelineBarbe <laloutrecosmique@hotmail.fr>
2021-10-22 14:55:15 +0200

    HTML et un bouton pour lancer le jeu, problème persistant de l'erreur quand on rentre du texte

---
+++ allumettesCélineB/allumettes.py
@@ -0,0 +1,6 @@
+matchesNumber = 50
+
+def remove(numberRemoved) :
+   matchesNumber - numberRemoved = matchesNumber 
+   print("il reste " + matchesNumber)
+
--- allumettesCélineB/script.js
+++ allumettesCélineB/script.js
@@ -1,5 +1,3 @@
-
-
 let matchesNumber = 50;
 
 // enlever un certain nombre d'allumettes
@@ -10,16 +8,24 @@ function remove(numbRemoved) {
 
 // demander à l'utilisateur le nombre à retirer jusqu'à ce qu'il n'y ai plus d'allumettes
 // avec les règles du jeu incluses
-while (matchesNumber>0) {
-    let numbRemoved = prompt("Combien d'allumettes voulez vous retirer? Entrez un chiffre entre 1 et 6");
-    if (numbRemoved>6 || numbRemoved<1) {
-        alert("Vous devez entrer un chiffre entre 1 et 6.");
-    } else if (numbRemoved>matchesNumber) {
-        alert("Il ne reste pas suffisamment d'allumettes");
-    } else if(typeof(numbRemoved) != "number") {
-        alert("Rentrez uniquement des chiffres s'il vous plait");
-    } else {
-        remove(numbRemoved);
-    }
-}; 
+function game () {
+    while (matchesNumber>0) {
+        let numbRemoved = parseInt(prompt("Combien d'allumettes voulez vous retirer? Entrez un chiffre entre 1 et 6"));
+        if (numbRemoved>6 || numbRemoved<1) {
+            alert("Vous devez entrer un chiffre entre 1 et 6.");
+        } else if (numbRemoved > matchesNumber) {
+            alert("Il ne reste pas suffisamment d'allumettes");
+        } else if (numbRemoved == NaN) {
+            alert("N'écrivez pas en toutes lettres les chiffres que vous souhaitez entrer") }
+        else {
+            remove(numbRemoved);
+        }
+    }; 
+    console.log("La partie est terminée, vous avez gagné");
+};
+
+// la vérification sur le prompt ne fonctionne pas comme on parse le résultat, ce sera à arranger quand on rentrera avec un input directement dans le corps du html
+
+//notion de victoire
+
 
