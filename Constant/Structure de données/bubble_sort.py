b002c9663620e53f651a5ff51764c76875f1ac7a
Astating <cguilb3@gmail.com>
2021-11-05 16:32:04 +0100

    Correction Bubble sort : The Knuth Way

--- Constant/Structure de données/bubble_sort.py
+++ Constant/Structure de données/bubble_sort.py
@@ -1,5 +1,18 @@
 soap = [2, 54, 7, 38, 22, 13, 19, 45, 31, 77, 98, 5, 12, 28, 81, 26, 72, 4, 20, 42]
 
+for i in range(len(soap)-1, 1,-1):
+    for j in range(i-1):
+        if soap[j+1] < soap[j]:
+            soap[j+1], soap[j] = soap[j], soap[j+1]
+
+print(soap)
+
+
+"""
+NOT BUBBLE SORT !
+
+soap = [2, 54, 7, 38, 22, 13, 19, 45, 31, 77, 98, 5, 12, 28, 81, 26, 72, 4, 20, 42]
+
 for h in range(len(soap)-1):
   for i in range(len(soap)-1) :
     if soap[i] > soap[i+1]:
@@ -8,3 +21,4 @@ for h in range(len(soap)-1):
       soap[i+1] = temp
 
 print(soap)
+ """
